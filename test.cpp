1e5c46634bb65ea34963d1154d72647d3839569b
mirza-mohibul-hasan <mirzamohibul618@gmail.com>
2024-02-25 23:28:50 +0600

    initial stage

---
+++ test.cpp
@@ -0,0 +1,20 @@
+/*Mirza Mohibul Hasan*/
+/*CSE-19@JUST*/
+#include <bits/stdc++.h>
+using namespace std;
+#define pi acos(-1)
+#define mod 1000000007
+#define nl "\n"
+#define deb(x) cout << #x << "=" << x << '\n'
+#define fast_run ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr)
+int main()
+{
+    fast_run;
+    long long t;
+    cin >> t;
+    while (t--)
+    {
+      cout<<"Hello WOrld"<<nl;
+    }
+    return 0;
+}
